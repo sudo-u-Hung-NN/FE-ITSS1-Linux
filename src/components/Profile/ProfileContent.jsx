d84fca54d345438a95ec23c2a0cc665f16b87e7a
nguyentung2000 <ntttung6100@gmail.com>
2022-12-17 03:43:15 +0700

    Them avt

--- src/components/Profile/ProfileContent.jsx
+++ src/components/Profile/ProfileContent.jsx
@@ -1,62 +1,113 @@
-import React, { useState } from 'react'
-import '../../CSS/profile.css'
-import { BsFacebook, BsGoogle, BsTwitter } from 'react-icons/bs'
-import { AiOutlineClose } from 'react-icons/ai';
-import 'date-fns';
-import { MdFileUpload } from 'react-icons/md';
-import ProfileUpdateDetails from './ProfileUpdateDetails';
-import ProfileDetails from './ProfileDetails';
-import ProfileUpdatePassword from './ProfileUpdatePassword';
+import React, { useState } from "react";
+import "../../CSS/profile.css";
+import { BsFacebook, BsGoogle, BsTwitter } from "react-icons/bs";
+import { AiOutlineClose } from "react-icons/ai";
+import "date-fns";
+import { MdFileUpload } from "react-icons/md";
+import ProfileUpdateDetails from "./ProfileUpdateDetails";
+import ProfileDetails from "./ProfileDetails";
+import ProfileUpdatePassword from "./ProfileUpdatePassword";
+import { useDispatch, useSelector } from "react-redux";
+import { uploadImageToCloudinary } from "../Api/upload.api";
+import { updateUser } from "../Api/user.api";
+import { toast } from "react-toastify";
 
-const ProfileContent = ({setShow}) => {
-    const [updateDetails, setUpdateDetails] = useState(false);
-    const [updatePassword, setUpdatePassword] = useState(false);
-    return (
-        <div className='profile'>
-            <AiOutlineClose
-                fontSize={25}
-                id='icon-close-profile'
-                onClick={()=>setShow(false)}
-            />
-            <div className='profile-content'>
-                <div className='image'>
-                    <img src="https://cdnimg.vietnamplus.vn/uploaded/mzdic/2020_08_22/brunofernandes2208.jpg" alt="" />
-                    <MdFileUpload className='icon-upload-image' />
-                    {/* {currentUser.avatar} */}
-                </div>
-                <div className='media-icons'>
-                    <BsFacebook className='icon' fontSize={20} />
-                    <BsTwitter className='icon' fontSize={20} />
-                    <BsGoogle className='icon' fontSize={20} />
-                </div>
-                {
-                    updatePassword ?
-                        <ProfileUpdatePassword setUpdatePassword={setUpdatePassword} />
-                        :
-                        <>
-                            {
-                                updateDetails ? <ProfileUpdateDetails setUpdateDetails={setUpdateDetails} /> : <ProfileDetails />
-                            }
-                            <div className='button'>
-                                {
-                                    !updateDetails &&
-                                    <button type="button" onClick={() => setUpdateDetails(true)}>Update Your Profile</button>
-                                }
-                            </div>
-                        </>
-                }
-            </div>
-            {
-                !updatePassword &&
-                <span
-                    className='update-password'
-                    onClick={() => setUpdatePassword(true)}
-                >
-                    Update your password
-                </span>
+const ProfileContent = ({ setShow }) => {
+  const [updateDetails, setUpdateDetails] = useState(false);
+  const [updatePassword, setUpdatePassword] = useState(false);
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.login.currentUser);
+  const [image, setImage] = useState(user?.avatar);
+  const handleChangeImage = (e) => {
+    const formData = new FormData();
+    formData.append("file", e.target.files[0]);
+    uploadImageToCloudinary(formData)
+      .then((res) => {
+        return res.data.url;
+      })
+      .then((res) => {
+        setImage(res);
+        updateUser(
+          user?.id,
+          {
+            username: user?.name,
+            phone: user?.phone,
+            birth_date: user?.birth_date,
+            avatar: res,
+            gender: user?.gender,
+          },
+          dispatch,
+          toast
+        );
+      })
+      .catch((err) => {
+        console.error("Err: ", err);
+      });
+  };
+  return (
+    <div className="profile">
+      <AiOutlineClose
+        fontSize={25}
+        id="icon-close-profile"
+        onClick={() => setShow(false)}
+      />
+      <div className="profile-content">
+        <div className="image">
+          <img
+            src={
+              image ||
+              user?.avatar ||
+              `https://cdn-icons-png.flaticon.com/512/149/149071.png`
             }
-        </div>
-    )
-}
+            alt=""
+          />
+          <label htmlFor="avt">
+            <MdFileUpload className="icon-upload-image" />
+          </label>
+          <input
+            type="file"
+            name="avt"
+            id="avt"
+            className="profile-avt"
+            onChange={handleChangeImage}
+          />
 
-export default ProfileContent
+          {/* {currentUser.avatar} */}
+        </div>
+        <div className="media-icons">
+          <BsFacebook className="icon" fontSize={20} />
+          <BsTwitter className="icon" fontSize={20} />
+          <BsGoogle className="icon" fontSize={20} />
+        </div>
+        {updatePassword ? (
+          <ProfileUpdatePassword setUpdatePassword={setUpdatePassword} />
+        ) : (
+          <>
+            {updateDetails ? (
+              <ProfileUpdateDetails setUpdateDetails={setUpdateDetails} />
+            ) : (
+              <ProfileDetails />
+            )}
+            <div className="button">
+              {!updateDetails && (
+                <button type="button" onClick={() => setUpdateDetails(true)}>
+                  Update Your Profile
+                </button>
+              )}
+            </div>
+          </>
+        )}
+      </div>
+      {!updatePassword && (
+        <span
+          className="update-password"
+          onClick={() => setUpdatePassword(true)}
+        >
+          Update your password
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default ProfileContent;
