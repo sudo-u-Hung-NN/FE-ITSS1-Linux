1125e1c61d901da0f8074c0b78239bec9cc226e4
Aresky-T <tn6354103@gmail.com>
2023-01-10 09:01:48 +0700

    update vip 10/1 ver 2

--- src/components/Dish/DishOption/Description.jsx
+++ src/components/Dish/DishOption/Description.jsx
@@ -1,8 +1,7 @@
 import Parser from "html-react-parser";
 import React from "react";
-import Convert from "../../Api/translate.api"
 
-export const Description = ({dishData}) => {
+export const Description = ({ dishData }) => {
     return (
         <div className="description-0">
             <h3>Miêu tả</h3>
